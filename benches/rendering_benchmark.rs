468a8a1f8444a8b4a7e8802dddc940ed84219ced
Lloyd <lloydmeta@gmail.com>
2017-06-17 14:38:21 +0900

    Add a rendering test for 1000x1000

--- benches/rendering_benchmark.rs
+++ benches/rendering_benchmark.rs
@@ -24,3 +24,12 @@ fn render_500x500_grid_10times(b: &mut Bencher) {
                app.render()
            })
 }
+
+#[bench]
+fn render_1000x1000_grid_10times(b: &mut Bencher) {
+    let grid = Grid::new(1000, 1000);
+    let mut app = App::new(grid, 1024, 768, 30);
+    b.iter(|| for _ in 0..10 {
+               app.render()
+           })
+}
